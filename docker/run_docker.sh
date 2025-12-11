a7ddd31c567c139e45f483e2382eb56247ce1bf8
catnys <catnyseverdyn@gmail.com>
2025-12-12 00:02:23 +0300

    tests: update stuff

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -4,7 +4,7 @@ set -e
 IMAGE_NAME="gem5-universal:latest"
 DOCKERFILE="docker/dockerfile.dev"
 
-echo "🔧 Building Docker image if needed..."
+echo "🔧 Building Docker image..."
 docker buildx build --platform linux/amd64 -t $IMAGE_NAME -f $DOCKERFILE .
 
 echo "🚀 Starting container..."
